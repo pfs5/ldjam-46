cdaeb3f54f36642d22cc01d1a1c7bda91b67f8d7
gpetkovic <gpetkovic@gmail.com>
2020-04-18 17:02:17 +0200

    Adding source project and cleaning up

---
+++ Source/Game/OfficeZGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OfficeZGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_API AOfficeZGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
