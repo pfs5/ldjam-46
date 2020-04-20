62b35c9ee3cd37f94bdb20f02c80b01f5322f04a
_pfsfive <patrikfs5@gmail.com>
2020-04-20 18:59:55 +0200

    Added interaction bubble

--- Source/Game/Player/OfficeZPlayerController.cpp
+++ Source/Game/Player/OfficeZPlayerController.cpp
@@ -61,7 +61,7 @@ void AOfficeZPlayerController::Tick(float deltaSeconds)
 
 	MovePlayer();
 
-	UE_LOG(LogTemp, Warning, TEXT("inputs = %d"), _movementInputs.size());
+	//UE_LOG(LogTemp, Warning, TEXT("inputs = %d"), _movementInputs.size());
 	//UE_LOG(LogTemp, Warning, TEXT("movement = %s"), *_movementVector.ToString());
 	//UE_LOG(LogTemp, Warning, TEXT("dir = %d"), (int)_playerDirection);
 }
